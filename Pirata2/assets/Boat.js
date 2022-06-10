fada3e40b4b89e4a7d6ac31a89c75e8608151cbd
lL0dS <104173912+lL0dS@users.noreply.github.com>
2022-06-09 21:04:39 -0300

    Add files via upload

---
+++ Pirata2/assets/Boat.js
@@ -0,0 +1,45 @@
+class Boat {
+  constructor(x, y, width, height, boatPos) {
+    var options = {
+      restitution: 0.8,
+      friction: 1.0,
+      density: 1.0,
+    };
+    this.animation = boatAnimation
+
+    this.speed = 0.5;
+
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+
+    this.boatPosition = boatPos;
+    //this.image = loadImage("assets/boat.png");
+    World.add(world, this.body);
+  }
+
+  animate(){
+    this.speed += 0.5;
+  }
+
+//remover navios do mundo
+  remove(index) {
+    Matter.World.remove(world, boats[index].body);
+    boats.splice(index, 1);
+
+    var index = floor(this.speed % this.animation.length);
+  }
+
+  display() {
+    var angle = this.body.angle;
+    var pos = this.body.position;
+  
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.animation(index), 0, this.boatPosition, this.width, this.height);
+    noTint();
+    pop();
+  }
+}
